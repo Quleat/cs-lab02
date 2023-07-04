325200c0d6f8ea2efbdbe40e90caff7b8658d10b
Slava (GamovVA) <kintoxo86@gmail.com>
2023-07-04 21:08:50 +0300

    code: added maximum

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 int main(){
 	cout <<  "Enter A and B: ";
@@ -8,4 +9,5 @@ int main(){
 	cout << "A - B = " << a - b << '\n';
 	cout << "A * B = " << a * b << '\n';
 	cout << "A / B = " << a / b << '\n';
+	cout << "Maximum = " << max(a, b) << '\n';
 }
